e7e9027b32d436e1b91011b54a8d0ea8938b531e
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-08-12 16:10:22 +0300

    Create basic login page
    For testing

---
+++ src/app/public/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: (err) => console.error('Login failed', err),
+    });
+  }
+}
