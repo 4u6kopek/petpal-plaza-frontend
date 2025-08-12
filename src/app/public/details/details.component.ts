70db8c9b57f14449e9726e992f03033d4dbd052d
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-08-13 01:45:41 +0300

    Upgrade details component to vizualise the tests better

--- src/app/public/details/details.component.ts
+++ src/app/public/details/details.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { Pet } from '../../models/pet.interface';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  template: '<p>Pet Details Page (ID: {{ id }})</p>',
+  imports: [],
+  templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
-  id = '';
+export class DetailsComponent implements OnInit {
+  id: string = '';
+  pet: Pet | undefined;
+
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    if (this.id) {
+      this.apiService.getPet(this.id).subscribe({
+        next: (pet) => (this.pet = pet),
+        error: (err) => console.error('Error fetching pet details', err),
+      });
+    }
+  }
 }
