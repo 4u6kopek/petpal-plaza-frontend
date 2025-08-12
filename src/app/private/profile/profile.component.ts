644268763803d659e61633bb967f439a2294102c
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-08-12 16:10:47 +0300

    Profile component for testing

---
+++ src/app/private/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  template: '<p>Welcome to your profile!</p>',
+  styleUrls: ['./profile.component.css'],
+})
+export class ProfileComponent {}
