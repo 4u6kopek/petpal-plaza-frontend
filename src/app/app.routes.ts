7d5ed7c12eb2beb6f64a550c8d417d14a14d08dc
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-08-13 00:11:56 +0300

    Route catalog and details components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { LoginComponent } from './public/login/login.component';
+import { CatalogComponent } from './public/catalog/catalog.component';
+import { DetailsComponent } from './public/details/details.component';
 import { authGuard } from './guards/auth.guard';
 import { guestGuard } from './guards/guest.guard';
 
@@ -8,6 +10,8 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent, canActivate: [guestGuard] },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'details/:id', component: DetailsComponent },
   {
     path: 'profile',
     loadComponent: () =>
