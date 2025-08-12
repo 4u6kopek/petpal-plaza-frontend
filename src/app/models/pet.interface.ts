c54a1d9e113b57fbb003152c8e0178cebb55c695
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-08-13 00:01:56 +0300

    Create pet interface

---
+++ src/app/models/pet.interface.ts
@@ -0,0 +1,10 @@
+export interface Pet {
+  _id?: string;
+  name: string;
+  species: string;
+  age: number;
+  description: string;
+  ownerId: string;
+  likes: string[];
+  comments: Comment[];
+}
