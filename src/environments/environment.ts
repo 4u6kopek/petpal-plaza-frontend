9430056b4b7d4f8c5fe52b177714cd7489b7f538
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-08-13 00:00:41 +0300

    Add local and apiUrl

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBsTIhZ64mrdkeM6jUMa0Q-23F1W4Tjw7M',
+    authDomain: 'petpal-plaza.firebaseapp.com',
+    projectId: 'petpal-plaza',
+    storageBucket: 'petpal-plaza.firebasestorage.app',
+    messagingSenderId: '418564109814',
+    appId: '1:418564109814:web:1e414b577fbd7199da43d7',
+    measurementId: 'G-E6RHQWMLYY',
+  },
+  apiUrl: 'http://localhost:5000/api',
+};
